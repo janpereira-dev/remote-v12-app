94baf7eab69f7c846f63764dd041114398d5ff8a
Jan Pereira <jan.pereira@outlook.com>
2025-10-08 13:37:55 +0200

    feat(v12): fix remote app

--- src/app/features/ventas/ventas.module.ts
+++ src/app/features/ventas/ventas.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { VentasRoutingModule } from './ventas-routing.module';
 import { VentasComponent } from './ventas.component';
-
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    VentasComponent
-  ],
-  imports: [
-    CommonModule,
-    VentasRoutingModule
-  ]
+  declarations: [VentasComponent],
+  imports: [CommonModule, VentasRoutingModule, MatButtonModule],
 })
-export class VentasModule { }
+export class VentasModule {}
