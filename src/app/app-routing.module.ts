5e766badfd6a101f0253e374d311e05cd468cf77
Jan Pereira <jan.pereira@outlook.com>
2025-10-08 11:35:23 +0200

    feat(v12): initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'ventas', loadChildren: () => import('./features/ventas/ventas.module').then(m => m.VentasModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
